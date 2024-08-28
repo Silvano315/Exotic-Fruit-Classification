dd560f1a7d03aad6a9adf90f33c26a874c59cb29
Silvano Quarto <silvano.quarto@gmail.com>
2024-08-28 15:31:21 +0200

    preprocessing and viz fx updated

---
+++ src/preprocessing_and_viz.py
@@ -0,0 +1,23 @@
+import pandas as pd
+import numpy as np
+from matplotlib import pyplot as plt
+import seaborn as sns
+import plotly.express as px
+
+
+# Function to check if there are duplicates in the dataset
+def handle_duplicates(df):
+    duplicates = df.duplicated().sum()
+    if duplicates > 0:
+        print(f"Number of duplicates: {duplicates}. Shape DF: {df.shape}")
+        df_cleaned = df.drop_duplicates()
+        print(f"Number of removed duplicates: {duplicates}. New shape DF: {df_cleaned.shape}")
+    else:
+        print("No duplicates found!")
+        df_cleaned = df
+    return df_cleaned
+
+
+# Function to perform EDA with plotly.express: box/violin plot and histogram with or without boxplot for comparison
+def plot_feature_distribution():
+    pass
