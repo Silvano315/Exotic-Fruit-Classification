1f8af5d37553cd25526a09e24b9d60d94bc1787b
Silvano Quarto <silvano.quarto@gmail.com>
2024-08-30 17:54:54 +0200

    viz fx updated

--- src/ml_functions.py
+++ src/ml_functions.py
@@ -2,12 +2,14 @@ import pandas as pd
 import numpy as np
 from matplotlib import pyplot as plt
 import seaborn as sns
+import plotly.graph_objects as go
+import plotly.express as px
 import os
 import warnings
 warnings.filterwarnings("ignore", category=RuntimeWarning)
-from sklearn.model_selection import train_test_split, GridSearchCV
+from sklearn.model_selection import train_test_split, GridSearchCV, StratifiedKFold
 from sklearn.neighbors import KNeighborsClassifier
-from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
+from sklearn.metrics import accuracy_score, classification_report, confusion_matrix, precision_score, recall_score, f1_score, log_loss
 
 from src.constants import RANDOM_SEED
 
@@ -105,3 +107,173 @@ def evaluate_model(model, X_test, y_test):
     plt.ylabel('Actual', labelpad=12)
     plt.title('Confusion Matrix')
     plt.show()
+
+
+# Function to performe a cross-validation using the number of neighbors for the best KNN model
+def cross_validation_model(df, target, n_neighbors, cv=5):
+
+    X = df.drop(columns=[target])
+    y = df[target]
+
+    skf = StratifiedKFold(n_splits=cv, shuffle=True, random_state=RANDOM_SEED)
+
+    metrics = {
+        'fold': [],
+        'train_accuracy': [], 'test_accuracy': [],
+        'train_precision': [], 'test_precision': [],
+        'train_recall': [], 'test_recall': [],
+        'train_f1': [], 'test_f1': [],
+        'train_loss': [], 'test_loss': []
+    }
+
+    print("="*40)
+    for fold, (train_index, test_index) in enumerate(skf.split(X,y)):
+
+        X_train, X_test = X.iloc[train_index], X.iloc[test_index]
+        y_train, y_test = y.iloc[train_index], y.iloc[test_index]
+
+        print(f"Training KNN in Fold {fold+1}...")
+        knn = KNeighborsClassifier(n_neighbors=n_neighbors)
+        knn.fit(X_train, y_train)
+
+        y_train_pred = knn.predict(X_train)
+        y_test_pred = knn.predict(X_test)
+        y_train_proba = knn.predict_proba(X_train)
+        y_test_proba = knn.predict_proba(X_test)
+
+        metrics['fold'].append(fold + 1)
+        metrics['train_accuracy'].append(accuracy_score(y_train, y_train_pred))
+        metrics['test_accuracy'].append(accuracy_score(y_test, y_test_pred))
+        metrics['train_precision'].append(precision_score(y_train, y_train_pred, average='weighted'))
+        metrics['test_precision'].append(precision_score(y_test, y_test_pred, average='weighted'))
+        metrics['train_recall'].append(recall_score(y_train, y_train_pred, average='weighted'))
+        metrics['test_recall'].append(recall_score(y_test, y_test_pred, average='weighted'))
+        metrics['train_f1'].append(f1_score(y_train, y_train_pred, average='weighted'))
+        metrics['test_f1'].append(f1_score(y_test, y_test_pred, average='weighted'))
+        metrics['train_loss'].append(log_loss(y_train, y_train_proba))
+        metrics['test_loss'].append(log_loss(y_test, y_test_proba))
+
+    print("="*40)
+    results_cv = pd.DataFrame(metrics)
+
+    return results_cv
+
+
+# Function to visualize scatter plot for a choosen metric over the folds
+def plot_line_accuracy_log_loss(results_df, metric = 'accuracy'):
+
+    fig = go.Figure()
+
+    fig.add_trace(go.Scatter(
+        x=results_df['fold'], 
+        y=results_df[f'train_{metric}'],
+        mode='lines+markers',
+        name=f'Train {metric.capitalize()}',
+        line=dict(color='blue'),
+        marker=dict(size=10)
+    ))
+
+    fig.add_trace(go.Scatter(
+        x=results_df['fold'], 
+        y=results_df[f'test_{metric}'],
+        mode='lines+markers',
+        name=f'Test {metric.capitalize()}',
+        line=dict(color='green'),
+        marker=dict(size=10)
+    ))
+
+    fig.update_layout(
+        title = f"{metric.capitalize()} over folds",
+        xaxis_title='Fold',
+        yaxis_title='Score',
+        legend_title='Metric',
+        template='plotly_white'
+    )
+
+    return fig
+
+
+# Function to visualize KNN performances for 'Accuracy', 'Precision', 'Recall', 'F1 Score' both train and test
+def plot_bar_metrics(results_df):
+    metrics_summary = {
+        'Metric Type': ['Accuracy', 'Precision', 'Recall', 'F1 Score'],
+        'Train Mean': [
+            results_df['train_accuracy'].mean(),
+            results_df['train_precision'].mean(),
+            results_df['train_recall'].mean(),
+            results_df['train_f1'].mean(),
+        ],
+        'Train Std': [
+            results_df['train_accuracy'].std(),
+            results_df['train_precision'].std(),
+            results_df['train_recall'].std(),
+            results_df['train_f1'].std(),
+        ],
+        'Test Mean': [
+            results_df['test_accuracy'].mean(),
+            results_df['test_precision'].mean(),
+            results_df['test_recall'].mean(),
+            results_df['test_f1'].mean(),
+        ],
+        'Test Std': [
+            results_df['test_accuracy'].std(),
+            results_df['test_precision'].std(),
+            results_df['test_recall'].std(),
+            results_df['test_f1'].std(),
+        ]
+    }
+    summary_df = pd.DataFrame(metrics_summary)
+
+    fig = go.Figure()
+
+    fig.add_trace(go.Bar(
+        x=summary_df['Metric Type'],
+        y=summary_df['Train Mean'],
+        name='Train',
+        error_y=dict(type='data', array=summary_df['Train Std'], visible=True),
+        marker_color='lightblue'
+    ))
+    fig.add_trace(go.Bar(
+        x=summary_df['Metric Type'],
+        y=summary_df['Test Mean'],
+        name='Test',
+        error_y=dict(type='data', array=summary_df['Test Std'], visible=True),
+        marker_color='salmon'
+    ))
+
+    fig.update_layout(
+        title='Mean and Standard Deviation of Metrics for Training and Test Sets',
+        xaxis_title='Metric Type',
+        yaxis_title='Score',
+        barmode='group',
+        template='plotly_white',
+        legend_title='Dataset'
+    )
+
+    return fig
+
+
+# Violin Plot for a choosen metric both train and test 
+def plot_violin_metric(results_df, metric_name):
+    valid_metrics = ['accuracy', 'precision', 'recall', 'f1', 'loss']
+    if metric_name not in valid_metrics:
+        raise ValueError(f"Invalid metric name. Choose from {valid_metrics}.")
+    
+    train_col = f'train_{metric_name}'
+    test_col = f'test_{metric_name}'
+    melted_df = pd.melt(results_df[['fold', train_col, test_col]], id_vars=['fold'], 
+                        value_vars=[train_col, test_col], 
+                        var_name='Dataset', value_name='Score')
+    melted_df['Dataset'] = melted_df['Dataset'].apply(lambda x: 'Train' if 'train' in x else 'Test')
+
+    fig = px.violin(melted_df, x='Dataset', y='Score', color='Dataset', 
+                    box=True, points='all',
+                    title=f'Violin Plot for {metric_name.capitalize()} Metric',
+                    template='plotly_white')
+
+    fig.update_layout(
+        yaxis_title=f'{metric_name.capitalize()} Score',
+        xaxis_title='Dataset',
+    )
+
+    return fig
